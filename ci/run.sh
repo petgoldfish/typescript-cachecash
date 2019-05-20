c65b75a12566828b5d73aff67ad98cfac8f99b0a
cacheflux <markus@cachecash.com>
2019-05-20 20:47:20 +0200

    Update integration tests for go-cachecash

--- ci/run.sh
+++ ci/run.sh
@@ -2,7 +2,7 @@
 set -xe
 
 case "$BUILD_MODE" in
-	test)
+    test)
         npm run test:prod
         npm run build
         ;;
@@ -11,9 +11,12 @@ case "$BUILD_MODE" in
         ssh-agent bash -c 'ssh-add id_deploykey_go-cachecash; git clone git@github.com:cachecashproject/go-cachecash.git'
 
         cd go-cachecash
-        docker-compose build
-        docker run --rm -v $PWD/cfg:/cfg go-cachecash_publisher generate-config -outputPath /cfg/ -upstream "http://upstream:80" -publisherCacheServiceAddr publisher:8082
+        echo "[*] Starting docker-compose deployment..."
+        #docker-compose build
         docker-compose up -d
+
+        echo "[*] Waiting until escrow is setup..."
+        while ! curl -v 'http://127.0.0.1:7100/info' | jq -e '.Escrows|length==1'; do sleep 10; done
         cd ..
 
         echo "[*] Waiting for network to become healthy..."
